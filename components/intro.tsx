1f134d358f9966ed9f1bbebd78f9f9b0ea64135b
ananyadas2607 <das.ananya2607@gmail.com>
2024-05-10 01:53:28 -0700

    commit-header

---
+++ components/intro.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function intro() {
+  return (
+    <section>
+        <div className='flex items-center justify-center'>
+            
+        </div>
+    </section>
+  )
+}
